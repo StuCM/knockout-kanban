e1c2e3e5726141b024d0939f8e110e52a525a257
stuart <stuartmarshall@fixie3d.com>
2024-01-23 16:09:36 +0000

    add json db with get and post requests and test

---
+++ src/store/fetchApi.js
@@ -0,0 +1,32 @@
+//get all tasks from db or get a task by id
+export async function getTasksFromDb(id) {
+    try {
+        if(id){
+            const response = fetch(`http://localhost:3000/tasks/${id}`)
+            return response.json();
+        } else {
+            const response = fetch(`http://localhost:3000/tasks`)
+            return response.json();
+        }
+    } catch (error) {
+        console.error('Error', error);
+        return null;
+    }
+}
+
+//add a task to db
+export async function addTaskToDb(task) {
+    try {
+        const response = await fetch(`http://localhost:3000/tasks`, {
+            method: 'POST',
+            body: JSON.stringify(task),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        return response.json();
+    } catch (error) {
+        console.error('Error', error);
+        return null;
+    }
+}
