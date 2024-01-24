94fa4a12f87598ac647472a1a5fe2b7581a0a22a
stuart <stuartmarshall@fixie3d.com>
2024-01-24 16:17:15 +0000

    add toggle for large and small tasks

--- src/components/app/app.viewModel.js
+++ src/components/app/app.viewModel.js
@@ -1,15 +1,20 @@
 import ko from 'knockout';
-import { connect } from 'knockout-store';
-import { fetchTasks } from '../../store/store.js';
+import { connect, setState } from 'knockout-store';
+import { fetchTasks, toggleTaskSizeLarge } from '../../store/store.js';
 
 function AppViewModel(params) {
     self.boards = params.boards;
+    self.taskSize = ko.observable(false);
+
+    self.taskSize.subscribe(function() {
+        toggleTaskSizeLarge(self.taskSize())
+    })
 
     fetchTasks();
 }
 
-function mapStateToParams({ boards }) {
-    return { boards };
+function mapStateToParams({ boards, taskSizeLarge }) {
+    return { boards, taskSizeLarge };
 }
 
 export default connect(mapStateToParams)(AppViewModel);
